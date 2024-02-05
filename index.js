828cd87d969f633c82a8d9ec294e7bbfd9c48d91
zipic <zpc23041988@gmail.com>
2024-02-05 22:35:15 +0200

    check

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const server = require('./app');
 const sequelize = require('./db/connection');
 const express = require('express');
 const cors = require('cors');
+// const PORT = 3005;
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({origin: '*'}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.queryInterface.showAllTables().then((tables) => {
-  if (tables.includes('users')) {
-    console.log('Tables already exist. Skipping database synchronization.');
-  } else {
-    sequelize.sync()
-      .then(() => {
-        console.log('Database and table created!');
-      })
-      .catch((error) => {
-        console.error('Error syncing database:', error);
-      });
-  }
-});
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database and table created!');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
