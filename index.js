1c1bfbba7be033e7932945e41b364614416f6ead
zipic <zpc23041988@gmail.com>
2024-02-06 09:08:37 +0200

    changed2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./app');
 const sequelize = require('./db/connection');
 const express = require('express');
 const cors = require('cors');
-// const PORT = 3005;
+const PORT = 3005;
 
 const app = express();
 
@@ -11,15 +11,14 @@ app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// sequelize.sync({ force: false })
-//   .then(() => {
-//     console.log('Database and table created!');
-//   })
-//   .catch((error) => {
-//     console.error('Error syncing database:', error);
-//   });
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database and table created!');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
 
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
