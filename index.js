ea3956535972cd63a0f8fd869a3fc88a0ae65988
zipic <zpc23041988@gmail.com>
2024-02-06 09:01:08 +0200

    change1

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('Database and table created!');
-  })
-  .catch((error) => {
-    console.error('Error syncing database:', error);
-  });
+// sequelize.sync({ force: false })
+//   .then(() => {
+//     console.log('Database and table created!');
+//   })
+//   .catch((error) => {
+//     console.error('Error syncing database:', error);
+//   });
 
 // server.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
